e1ecc808dc9bcd7f70a46f586119dd353dd58b94
Sami Viitanen <sami.viitanen@gmail.com>
2024-04-30 21:58:10 +0300

    averages

--- sendvalues.py
+++ sendvalues.py
@@ -5,35 +5,42 @@ import os
 import requests
 import time
 
-print('Starting SHT30 loop...')
-
 bus = smbus2.SMBus(1)
+lastTemp = -300.0
+lastHum = -1.0
 
 while True:
-    print('Reading values...')
+    tempSum = 0.0
+    humSum + 0.0
 
-    bus.write_i2c_block_data(0x44, 0x2C, [0x06])
-    data = bus.read_i2c_block_data(0x44, 0x00, 6)
-    cTemp = ((((data[0] * 256.0) + data[1]) * 175) / 65535.0) - 45
-    humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
+    for x in range(0, 5):
+        bus.write_i2c_block_data(0x44, 0x2C, [0x06])
+        data = bus.read_i2c_block_data(0x44, 0x00, 6)
+        tempSum += ((((data[0] * 256.0) + data[1]) * 175) / 65535.0) - 45
+        humSum += 100 * (data[3] * 256 + data[4]) / 65535.0
+    	time.sleep(2)
+
+    cTemp = round(tempSum / 5.0, 1)
+    humidity = round(humSum / 5.0)
+
     timestamp = datetime.datetime.utcnow().isoformat()
-
     token = os.environ.get('SUPERVISOR_TOKEN','?')
-
-    print('Sending Temperature: {:.2f} \N{DEGREE SIGN}C'.format(cTemp))
-    url = 'http://supervisor/core/api/states/sensor.sht30_temperature'
     headers = {'Content-type': 'application/json', 'Authorization': 'Bearer ' + token }
-    data = {'entity': 'sensor.sht30_temperature', 'attributes': { 'unit_of_measurement': '\N{DEGREE SIGN}C'}}
-    data['state'] = round(cTemp,2)
-    data['last_updated'] = timestamp
-    response = requests.post(url, data=json.dumps(data), headers=headers)
 
-    print('Swnding humidity: {:.2f} %'.format(humidity))
-    url = 'http://supervisor/core/api/states/sensor.sht30_humidity'
-    data = {'entity': 'sensor.sht30_humidity', 'attributes': { 'unit_of_measurement': '%'}}
-    data['state'] = round(humidity,2)
-    data['last_updated'] = timestamp
-    response = requests.post(url, data=json.dumps(data), headers=headers)
+    if lastTemp != cTemp:
+        url = 'http://supervisor/core/api/states/sensor.sht30_temperature'
+        data = {'entity': 'sensor.sht30_temperature', 'attributes': { 'unit_of_measurement': '\N{DEGREE SIGN}C'}}
+        data['state'] = cTemp
+        data['last_updated'] = timestamp
+        response = requests.post(url, data=json.dumps(data), headers=headers)
+        lastTemp = cTemp
 
-    print('All done. Now sleep.')
-    time.sleep(60)
+    if lastHum != humidity:
+        url = 'http://supervisor/core/api/states/sensor.sht30_humidity'
+        data = {'entity': 'sensor.sht30_humidity', 'attributes': { 'unit_of_measurement': '%'}}
+        data['state'] = humidity
+        data['last_updated'] = timestamp
+        response = requests.post(url, data=json.dumps(data), headers=headers)
+        lastHum = humidity
+
+    time.sleep(50)
