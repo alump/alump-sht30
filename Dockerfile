7ea8341e28f8c004a1ae5d3d03406ad8521847ff
Sami Viitanen <sami.viitanen@mavericks.fi>
2024-04-30 13:26:47 +0300

    Runs and generates output, missing still actual sensor parts

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 ARG BUILD_FROM
 FROM $BUILD_FROM
 RUN apk add --no-cache python3 py3-pip
-RUN pip3 install smbus2 sht20
+RUN python3 -m venv /venv
+ENV PATH /venv/bin:$PATH
+RUN pip3 install smbus2
 COPY test.py /
 COPY run.sh /
 RUN chmod a+x /run.sh
--- test.py
+++ test.py
@@ -1,6 +1,12 @@
-from sht20 import SHT20
-sht = SHT20(1, resolution=SHT20.TEMP_RES_14bit)
-temp = sht.read_temp()
-humid = sht.read_humid()
-print("Temperature (°C): " + str(temp))
-print("Humidity (%RH): " + str(humid))
+import smbus2
+
+bus = smbus2.SMBus(1)
+bus.write_i2c_block_data(0x44, 0x2C, [0x06])
+data = bus.read_i2c_block_data(0x44, 0x00, 6)
+cTemp = ((((data[0] * 256.0) + data[1]) * 175) / 65535.0) - 45
+humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
+
+hStr = 'Relative humidity: {:.2f} %'.format(humidity)
+tStr = 'Temperature: {:.2f} \N{DEGREE SIGN}C'.format(cTemp)
+print(hStr)
+print(tStr)
